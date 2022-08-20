cfef69a4df129e27b3a230ac99d424e458f14092
The-Great1 <54747205+The-Great1@users.noreply.github.com>
2022-08-20 18:53:57 +0100

    Added SQL fundamentals

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- script that removes all records based on specific condition
+
+DELETE FROM `second_table`
+WHERE `score` <= 5;
+
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- script that computes the score average of all records in table
+
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
+
